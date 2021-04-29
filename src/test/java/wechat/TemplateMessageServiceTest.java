4407b2e08615397c5299161441cb8e2c4e923e85
limengning <micheal_li@foxmail.com>
2021-04-29 09:43:19 +0800

    Clear import.

--- src/test/java/wechat/TemplateMessageServiceTest.java
+++ src/test/java/wechat/TemplateMessageServiceTest.java
@@ -1,7 +1,6 @@
 package wechat;
 
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.HashMap;
 import java.util.Map;
